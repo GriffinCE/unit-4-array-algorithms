69a7d73459f001fd8861cdd4478726c965112c68
GriffinCE <griffinedson@gmail.com>
2026-02-26 09:18:14 -0800

    changed it to ArrayList

---
+++ ArrayListAlgorithms.java
@@ -0,0 +1,92 @@
+import java.util.ArrayList;
+
+public class ArrayListAlgorithms {
+   // DO NOT CHANGE THE VARIABLE NAME
+   private ArrayList<Integer> intArray;
+
+   /*
+   * ASSUMPTIONS FOR THIS ASSIGNMENT:
+   * 1) arraySize will always be >= 1
+   * 2) Random numbers should be in the range 0 - 99 (inclusive)
+   */
+
+   public ArrayListAlgorithms() {
+      // REQUIRED: initialize intArray to be of size arraySize
+      ArrayList<Integer> intArray = new ArrayList<Integer>();
+   }
+
+   public void populateArrayWithRandom() {
+      // REQUIRED: populate intArray with random integers from 0 - 99
+      int randomNum = (int) (Math.random() * 100);
+      for (int i = 0; i < randomNum; i++) {
+         intArray.add((int) (Math.random() * 100));
+      }
+   }
+
+   public void populateArrayWithSequential(int startNum) {
+      // REQUIRED: populate intArray with sequential integers starting at startNum
+      int startNumber = startNum;
+      int randomNum = (int) (Math.random() * 100);
+      for (int i = 0; i < randomNum; i++) {
+         intArray.add(startNumber);
+         startNumber++;
+      }
+   }
+
+   public int findMax() {
+      // REQUIRED: return the largest integer in intArray
+      int max = -99999999;
+      for (int i = 0; i < intArray.size(); i++) {
+         if (intArray.get(i) > max) {
+            max = intArray.get(i);
+         }
+      }
+      return max;
+   }
+
+   public boolean hasDuplicates() {
+      // REQUIRED: return true if there are duplicate values in the array
+      // HINT: use a nested for loop
+      int num = 101;
+      for (int i  = 0; i < intArray.size(); i++) {
+         num = intArray.get(i);
+         for (int j = i + 1; j <intArray.size(); j++) {
+            if (num == intArray.get(j)) {
+               return true;
+            }
+         }
+      }
+      return false;
+   }
+
+   public boolean isInArray(int intToFind) {
+      // REQUIRED: return true if intToFind is in intArray.
+      // NOTE: Use an enhanced for loop for this method
+      System.out.print("Is the number " + intToFind + " in the array? ");
+      for (int value : intArray) {
+         if (intToFind == value) {
+            return true;
+         }
+      }
+      return false;
+   }
+   public void printArray() {
+      System.out.print("Numbers in the array: ");
+      for (int value : intArray) {
+         System.out.print(value + " ");
+      }
+   }
+
+   public static void main(String[] args) {
+      // REQUIRED:
+      // 1) Create an ArrayAlgorithms object
+      // 2) Populate the array with random numbers
+      // 3) Call and print the result of EACH REQUIRED method
+      ArrayAlgorithms newArray = new ArrayAlgorithms(25);
+      newArray.populateArrayWithRandom();
+      newArray.printArray();
+      System.out.println("\nThe highest number in the array is " + newArray.findMax());
+      System.out.println("Does the array have duplicates? " + newArray.hasDuplicates());
+      System.out.println(newArray.isInArray(49));
+   }
+}
